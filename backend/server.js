80c6936999626560c75f24f2565e4f50da43ae17
jyen <joyhengg@gmail.com>
2023-09-30 03:22:05 +0800

    stepbystep

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://the-local-choice-2.vercel.app/"
+    "https://the-local-choice-2.vercel.app"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
