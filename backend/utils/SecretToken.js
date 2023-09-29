6213c9a215026214039687ce9f7194d70b74b9ea
jyen <joyhengg@gmail.com>
2023-09-30 02:40:37 +0800

    stepbystep

--- backend/utils/SecretToken.js
+++ backend/utils/SecretToken.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const {createSecretToken} = require("../utils/SecretToken");
+// const {createSecretToken} = require("../utils/SecretToken");
 
 module.exports.createSecretToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.TOKEN_KEY, {
